280f7aaffa679678de32b2c02e1d058ccca17403
ChequedMetal <kostiaandres@gmail.com>
2024-11-19 15:45:02 -0300

    si?

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       downloadButton.style.display = "block";
       downloadButton.textContent = "Descargar para Android";
       downloadButton.onclick = () => {
-        window.location.href = "./apk/app-release.apk";
+        window.location.href = "https://drive.google.com/uc?id=1rwf0pbsSIoVogU5jLrXrD1YnvAOhaK_O&export=download";
       };
     }
   });
