2e6af93536036bf3ed8673f149dd5165a8bb6ebb
ChequedMetal <kostiaandres@gmail.com>
2024-11-19 15:33:08 -0300

    ahora si

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       downloadButton.style.display = "block";
       downloadButton.textContent = "Descargar para Android";
       downloadButton.onclick = () => {
-        window.location.href = "https://www.mediafire.com/file/knmhm1uh19fcu65/app-release.apk/file"; // Cambia el enlace por el de tu APK
+        window.location.href = "./apk/app-release.apk";
       };
     }
   });
