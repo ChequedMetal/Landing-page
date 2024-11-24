67f06d4e74e0f9deb88aa715551362c5c5cbc622
ChequedMetal <kostiaandres@gmail.com>
2024-11-24 18:51:27 -0300

    V3.0.0

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const downloadButton = document.getElementById("download-button");
-  
-    // Detectar dispositivo Android
-    const isAndroid = /Android/i.test(navigator.userAgent);
-  
-    if (isAndroid) {
-      downloadButton.style.display = "block";
-      downloadButton.textContent = "Descargar para Android";
-      downloadButton.onclick = () => {
-        window.location.href = "https://drive.google.com/uc?id=1rCisPAlZ-fGJyE_771gHtEPMuTIH-PGQ&export=download";
-      };
-    }
-  });
-  
+  const downloadButton = document.getElementById("download-button");
+
+  // Detectar dispositivo Android
+  const isAndroid = /Android/i.test(navigator.userAgent);
+
+  if (isAndroid) {
+    downloadButton.style.display = "inline-block";
+    downloadButton.href = "https://drive.google.com/uc?id=1rCisPAlZ-fGJyE_771gHtEPMuTIH-PGQ&export=download"; 
+  } else {
+    downloadButton.style.display = "none";
+  }
+});
